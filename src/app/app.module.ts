d31f1dcf504076bc0afa2d19aa254ca402b82a49
Siddhesh Thadeshwar <sid@stringzweb.graphics>
2017-04-03 21:01:34 +0530

    Adding Routing, Adding Dashboard page, Revising HeroDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,37 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroesComponent, HeroDetailComponent ],
+  imports:      [ BrowserModule, FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
+  ],
+  declarations: [ AppComponent, DashboardComponent, HeroesComponent, HeroDetailComponent ],
   providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,20 +1,37 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from './hero';
+// Keep the Input import for now, you'll remove it later:
+import { Component, Input, OnInit } from '@angular/core';
+// The new HeroDetailComponent should take the id parameter from the params
+//   Observable in the ActivatedRoute service and use the HeroService to fetch
+//   the hero with that id.
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
 
+// Importing switchMap for use with Observable
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service';
+import { Hero } from './hero';
 
 @Component({
   selector: 'hero-detail',
-  template: `
-    <div *ngIf="hero">
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-      </div>
-    </div>
-  `
+  templateUrl: './hero-detail.component.html'
 })
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap( (params: Params) => this.heroService.getHero(+params['id']) )
+      .subscribe(hero => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
   @Input() hero: Hero;
 }
