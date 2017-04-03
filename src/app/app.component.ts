0b0e6463da4c483d06955b5364a3999067580184
Siddhesh Thadeshwar <sid@stringzweb.graphics>
2017-04-03 14:45:37 +0530

    Adding List of Heroes and displaying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,33 @@ import { Component } from '@angular/core';
   selector: 'my-app',
 	template: `
     <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
   `
 })
 export class AppComponent {
   title = "Tour of Heroes";
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm"
-  };
+  heroes = HEROES;
 }
 
 export class Hero {
   id: number;
   name: string;
 }
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
