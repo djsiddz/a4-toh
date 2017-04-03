97de522e5ead82aeb95cee9a7c7f5dacad673cec
Siddhesh Thadeshwar <sid@stringzweb.graphics>
2017-04-03 15:06:38 +0530

    Select and edit Hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,18 @@ import { Component } from '@angular/core';
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes">
+      <li *ngFor="let hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
+    <div *ngIf="selectedHero">
+      <h2>{{selectedHero.name}} details!</h2>
+      <div><label>id: </label>{{selectedHero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+      </div>
+    </div>
   `,
   styles: [`
     .selected {
@@ -64,6 +72,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = "Tour of Heroes";
   heroes = HEROES;
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
 
 export class Hero {
